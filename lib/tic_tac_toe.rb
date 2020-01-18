d22a7dd4741eb858f6d6a46def293f8c156e3326
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-01-18 15:04:37 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -93,6 +93,7 @@ class TicTacToe
  def winner
    WIN_COMBINATIONS.detect do |win_combo|
      if (@board[win_combo[0]])== "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]])== "X"
+       return "X"
          
          
   
