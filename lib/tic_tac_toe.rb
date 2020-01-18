31d4126c22a3aab28d85a1369f8b1b4713c91400
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-01-18 15:10:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -105,9 +105,11 @@ end
 def play 
   while over? == false 
   turn
+  end 
+    if won? 
+    puts "Congratulations #{winner}!"
+    elsif draw?
+    puts "Cat's Game!"
+    end 
 end 
-if won? 
-  puts "Congratulations #{winner}!"
-  elsif draw?
-  puts "Cat's Game!"
 end 
